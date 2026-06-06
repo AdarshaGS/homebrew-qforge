57d44a32eb28f702abc0d1e9a58756166cd8f7f6
AdarshaGS <adarshgs1928@gmail.com>
2026-06-06 15:23:47 +0530

    Remove deprecated macOS dependency

--- Casks/qforge.rb
+++ Casks/qforge.rb
@@ -2,7 +2,7 @@ cask "qforge" do
   version "1.0.0"
   sha256 "a2374b041f831c84fe4703fa188e9a0fbf91365b8080493adb13e2da95af3be9"
 
-  url "https://github.com/AdarshaGS/QForge/releases/download/v#{version}/QForge.dmg"
+  url "https://github.com/AdarshaGS/QForge/releases/download/v#{version}/SQL-Workbench.dmg"
 
   name "QForge"
   desc "Lightweight native database client for developers"
