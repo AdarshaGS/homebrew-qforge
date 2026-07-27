37a5e4a55f3bec8f3589569a652c9e1cb229789d
adarsh <52464965+AdarshaGS@users.noreply.github.com>
2026-07-27 16:22:50 +0530

    Deployment v1.0.9

--- Casks/qforge.rb
+++ Casks/qforge.rb
@@ -1,6 +1,6 @@
 cask "qforge" do
   version "1.0.9"
-  sha256 "d7dfcb905ee0be1737e2e5ce27035bdc6ba59aaaf7d03301825e4f8bb037e300"
+  sha256 "4d6f214c9b9da403ccd0fc90319fe99d5fe604a8c2c5dc1285fa423b2f7fd3c1"
 
   url "https://github.com/AdarshaGS/QForge/releases/download/v#{version}/QForge.dmg"
   name "QForge"
